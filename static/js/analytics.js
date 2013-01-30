5e24e7a1a8d371431ddf28e2610cc940405c005e
Connor Mendenhall <ecmendenhall@gmail.com>
2013-01-30 14:10:00 -0700

    More analytics updates.

--- static/js/analytics.js
+++ static/js/analytics.js
@@ -2,7 +2,4 @@ define(["http://www.google-analytics.com/ga.js"], function () {
     var _gaq = _gaq || [];
     _gaq.push(['_setAccount', 'UA-38081658-1']);
     _gaq.push(['_trackPageview']);
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 });
